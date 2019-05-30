c8306ff0958db9ee7a9fe1cb0b4631fa94a47e94
Dankore <zimmazone@yahoo.com>
2019-05-30 12:01:20 +0000

    add toggle and tooltip

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-const modalButton = document.querySelector('#modal-button');
+let modalButton = document.querySelector('#modal-button');
 const modal = document.querySelector('#modal');
 const overlay = document.querySelector('.modal-overlay')
 
-//Open modal
-modalButton.addEventListener('click', (e) => {
-    modal.classList.add('active');
-    overlay.classList.add('active');
-    modalButton.innerHTML = 'Close Modal';
-    // if (modalButton.innerHTML === 'Close Modal') {
-    //     modal.classList.remove('active');
-    //   overlay.classList.remove('active');
-    // } else {
-        
-    // }
+//Toggle
+ modalButton.addEventListener('click', () => {
+        if (modalButton.innerHTML === 'Open Modal'){
+            modal.classList.add('active');
+            overlay.classList.add('active');
+            modalButton.innerHTML = 'Close Modal'
+            } else {
+            modal.classList.remove('active');
+            overlay.classList.remove('active');
+            modalButton.innerHTML = 'Open Modal'
+                }
+        });
 
-})
 
 //Close modal
 const modalCloseButton = document.querySelector('.modal-close-button');
@@ -24,9 +24,12 @@ modalCloseButton.addEventListener('click', closeModal);
 function closeModal() {
     modal.classList.remove('active');
     overlay.classList.remove('active');
+    modalButton.innerHTML = 'Open Modal'
+
 };
 
 //Close modal on backdrop
 overlay.addEventListener('click', (e) => {
     closeModal();
+    modalButton.innerHTML = 'Open Modal'
 })
