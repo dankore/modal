0432809e50b70de554e5a774d6cb29fcdf8f1504
Dankore <zimmazone@yahoo.com>
2019-05-31 03:09:30 +0000

    refactored modal button js

--- main.js
+++ main.js
@@ -3,18 +3,29 @@ const modal = document.querySelector('#modal');
 const overlay = document.querySelector('.modal-overlay')
 
 //Toggle - 1st 
- modalButton.addEventListener('click', () => {
-        if (modalButton.innerHTML === 'Open Modal'){
+//  modalButton.addEventListener('click', () => {
+//         if (modalButton.innerHTML === 'Open Modal'){
+//             modal.classList.add('active');
+//             overlay.classList.add('active');
+//             modalButton.innerHTML = 'Close Modal'
+//             } else {
+//             modal.classList.remove('active');
+//             overlay.classList.remove('active');
+//             modalButton.innerHTML = 'Open Modal'
+//                 }
+//         });
+
+modalButton.addEventListener('click', () => {
+        if (modal.classList.contains('active')){
+              modal.classList.remove('active');
+            overlay.classList.remove('active');
+            modalButton.innerHTML = 'Open Modal'
+            } else {
             modal.classList.add('active');
             overlay.classList.add('active');
             modalButton.innerHTML = 'Close Modal'
-            } else {
-            modal.classList.remove('active');
-            overlay.classList.remove('active');
-            modalButton.innerHTML = 'Open Modal'
                 }
         });
-
 
 //Close modal - 2nd
 const modalCloseButton = document.querySelector('.modal-close-button');
